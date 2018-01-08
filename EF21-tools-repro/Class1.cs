5dc40d17e0d7a3e2dc90ff66a7123d18fecc45e4
Brad Barnich <brad@careevolution.com>
2018-01-08 16:39:08 -0500

    Add project files.

---
+++ EF21-tools-repro/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF21_tools_repro
+{
+    public class Class1
+    {
+    }
+}
